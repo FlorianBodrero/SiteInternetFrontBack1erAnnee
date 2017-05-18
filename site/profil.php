e888919e192c94883f3136446d14bb4e0f1f6cad
Romain <romain.lacube@gmail.com>
2017-05-18 17:19:40 +0200

    Exo5 - photo profil terminé

--- site/profil.php
+++ site/profil.php
@@ -1,20 +1,22 @@
 <?php require_once("inc/init.inc.php");
 
 //-----------------------TRAITEMENT PHP
-if(!internauteEstConnecte()){
+if (!internauteEstConnecte()) {
     header("location:connexion.php");
 }
 $pseudo = $_SESSION['membre']['pseudo'];
-$contenu .= '<p class="centre">Bonjour <strong>'.$_SESSION['membre']['pseudo']
-    .'</strong></p>';
+
+
+$contenu .= '<p class="centre">Bonjour <strong>' . $_SESSION['membre']['pseudo']
+    . '</strong></p>';
 $contenu .= '<div class="cadre"><h2>Voici vos informations :</h2></div>';
-$contenu .= 'Votre email est :'.$_SESSION['membre']['email'].'<br/>';
-$contenu .= 'Votre ville est :'.$_SESSION['membre']['ville'].'<br/>';
-$contenu .= 'Votre code postal est :'.$_SESSION['membre']['code_postal'].'<br/>';
-$contenu .= 'Votre adresse est :'.$_SESSION['membre']['adresse'].'<br/>';
+$contenu .= 'Votre email est :' . $_SESSION['membre']['email'] . '<br/>';
+$contenu .= 'Votre ville est :' . $_SESSION['membre']['ville'] . '<br/>';
+$contenu .= 'Votre code postal est :' . $_SESSION['membre']['code_postal'] . '<br/>';
+$contenu .= 'Votre adresse est :' . $_SESSION['membre']['adresse'] . '<br/>';
 $contenu .= '<form method="post" action=""><input name="desincription" value="Se désinscrire" type="submit"></form>';
 
-if($_POST){
+if ($_POST) {
     executeRequete("DELETE FROM membre WHERE pseudo = '$pseudo'");
 
     header("location:inscription.php");
@@ -22,21 +24,33 @@ if($_POST){
 }
 
 require_once("inc/haut.inc.php");
-echo $contenu;
 
+$query = executeRequete("SELECT id_membre from imageAvatar where id_membre='" . $_SESSION['membre']['id_membre'] . "'");
+if ($query->num_rows == 1) {
+    ?>
+    <div id="avaterContainer" style="text-align: center">
+        <img style="height: 100px; border-radius: 100%"
+             src="<?php echo "inc/img/photoAvatar/" . ($_SESSION['membre']['id_membre'] . ".jpg"); ?>">
+    </div>
+
+    <?php
+}
+
+
+echo $contenu;
 
 
 ?>
 
 
-<!--    EXO 5 - AJOUT PHOTO POUR AVATAR-->
-<div style="text-align: right">
-    <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
-        <h3> Ajout image avatar pour votre profil</h3>
-        <input type="file" name="photo" /> <br/>
-        <input type="submit" />
-    </form>
-</div>
+    <!--    EXO 5 - AJOUT PHOTO POUR AVATAR-->
+    <div style="text-align: right">
+        <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+            <h3> Ajout image avatar pour votre profil</h3>
+            <input type="file" name="photo"/> <br/>
+            <input type="submit"/>
+        </form>
+    </div>
 
 
 <?php
@@ -49,34 +63,32 @@ if (isset($_FILES['photo']) AND !empty($_FILES['photo'])) {
         if (in_array($extensionUpload, $fileExtension)) { // on teste si l'extension est bonne
             $chemin = "inc/img/photoAvatar/";
 
-            $resultat = move_uploaded_file($_FILES['photo']['tmp_name'], $chemin.$_SESSION['membre']['id_membre'].".".$extensionUpload );
+            $resultat = move_uploaded_file($_FILES['photo']['tmp_name'], $chemin . $_SESSION['membre']['id_membre'] . "." . $extensionUpload);
 
             if ($resultat) {
-                $query = executeRequete("SELECT id_membre from imageAvatar where id_membre='".$_SESSION['membre']['id_membre']."'");
+                $query = executeRequete("SELECT id_membre from imageAvatar where id_membre='" . $_SESSION['membre']['id_membre'] . "'");
 
-                if ($query -> num_rows == 0) {
-                    executeRequete("INSERT INTO imageavatar (id_membre, image) VALUE ('".$_SESSION['membre']['id_membre']."','".$_SESSION['membre']['id_membre'].".".$extensionUpload."')");
+                if ($query->num_rows == 0) {
+                    executeRequete("INSERT INTO imageavatar (id_membre, image) VALUE ('" . $_SESSION['membre']['id_membre'] . "','" . $_SESSION['membre']['id_membre'] . "." . $extensionUpload . "')");
                     echo 'Nous avons bien pris en compte votre image';
                 } else {
-                    executeRequete("UPDATE imageavatar SET image = ('".$_SESSION['membre']['id_membre'].".".$extensionUpload."') WHERE id_membre = '".$_SESSION['membre']['id_membre']."'" );
+                    executeRequete("UPDATE imageavatar SET image = ('" . $_SESSION['membre']['id_membre'] . "." . $extensionUpload . "') WHERE id_membre = '" . $_SESSION['membre']['id_membre'] . "'");
                     echo 'Votre image a ete mise a jour ! ';
                 }
 
             } else {
-                echo 'il y a eu une erreur durant l\'importation de votre photo de profil. Voici l\'erreur : '. $mysqli -> error;
+                echo 'il y a eu une erreur durant l\'importation de votre photo de profil. Voici l\'erreur : ' . $mysqli->error;
             }
 
         } else {
             echo 'Votre photo d\'avater doit etre au bon format : jpg, jpeg, ou png';
         }
-    
+
     } else {
         echo 'Votre image doit avoir une taille inférieur à 2Mo';
     }
 
 }
-
-
 
 
 require_once("inc/bas.inc.php");
