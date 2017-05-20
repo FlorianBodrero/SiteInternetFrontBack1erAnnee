59389bf30fb4dc21500a577978fbad71fc53ff55
Romain <romain.lacube@gmail.com>
2017-05-20 09:25:29 +0200

    Exo5

--- site/profil.php
+++ site/profil.php
@@ -31,7 +31,6 @@ require_once("inc/haut.inc.php");
 
 
 
-
 echo $contenu;
 $resul = executeRequete("SELECT image from imageavatar WHERE id_membre = (".$_SESSION['membre']['id_membre'].")"); // problème a régler ici
 $query = executeRequete("SELECT id_membre from imageAvatar where id_membre='" . $_SESSION['membre']['id_membre'] . "'");
