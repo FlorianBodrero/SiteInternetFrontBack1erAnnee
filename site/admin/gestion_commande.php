e77523f746bc1a7bbb2aa1511b9ae6e33cd25485
TheHiveFactory <nukacolaquartz@gmail.com>
2017-05-20 15:05:50 +0200

    Push avant modif

--- site/admin/gestion_commande.php
+++ site/admin/gestion_commande.php
@@ -7,8 +7,29 @@ if (!internauteEstConnecteEtEstAdmin()) {
 
 require_once("../inc/haut.inc.php");
 
-$commandes = executeRequete("SELECT * FROM commande");?>
+$commandes = executeRequete("SELECT commande.id_commande, commande.date_enregistrement, commande.montant, produit.id_produit, produit.titre, produit.photo, details_commande.quantite, membre.pseudo, membre.adresse, membre.ville, membre.code_postal, commande.etat
+    FROM commande, produit, membre, details_commande
+    WHERE commande.id_commande = details_commande.id_commande AND produit.id_produit = details_commande.id_produit
+    -- GROUP BY commande.id_commande, produit.id_produit, membre.pseudo
+    ");
 
- <h1>Bienvenu sur e-Ynov</h1>
+    $contenu .= '<table border="1"><tr>';
 
- <?php require_once("../inc/bas.inc.php"); ?>
+    while ($colonne = $commandes->fetch_field()) {
+        $contenu .= '<th>' . $colonne->name . ' </th>';
+    }
+    while ($ligne = $commandes->fetch_assoc()) { # parcours les elements de ma ligne
+        $contenu .= '<tr>';
+        foreach ($ligne as $item => $value) {
+            if ($item == "photo") {
+                $contenu .= '<td> <img src="../inc/img/photoProduit/' . $value . '" width = "70px" height = "70px"/></td>';
+            } else {
+                $contenu .= '<td>' . $value . '</td>';
+            }
+        }
+    }
+    $contenu .= '</table>';
+    echo $contenu;?>
+
+
+    <?php require_once("../inc/bas.inc.php"); ?>
